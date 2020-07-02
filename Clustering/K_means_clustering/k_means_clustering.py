6e4646500dcf53d77fc0ebbb2332f8e52e3cad35
Rahul Kumar <rahulkr9622@gmail.com>
2020-07-02 21:54:13 +0530

    Add clusters complete with image

--- Clustering/K_means_clustering/k_means_clustering.py
+++ Clustering/K_means_clustering/k_means_clustering.py
@@ -6,7 +6,6 @@ Created on Wed Jul  1 20:42:26 2020
 """
 
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
@@ -26,3 +25,26 @@ plt.ylabel('WCSS')
 plt.show()
 
 print(kmeans.cluster_centers_)
+
+kmeans = KMeans(n_clusters=5, init='k-means++', random_state=42)
+y_kmeans = kmeans.fit_predict(x)
+
+print(y_kmeans)
+
+plt.scatter(x[y_kmeans == 0, 0], x[y_kmeans == 0, 1], s=100, c='red',
+            label='Cluster 1')
+plt.scatter(x[y_kmeans == 1, 0], x[y_kmeans == 1, 1], s=100, c='blue',
+            label='Cluster 2')
+plt.scatter(x[y_kmeans == 2, 0], x[y_kmeans == 2, 1], s=100, c='green',
+            label='Cluster 3')
+plt.scatter(x[y_kmeans == 3, 0], x[y_kmeans == 3, 1], s=100, c='cyan',
+            label='Cluster 4')
+plt.scatter(x[y_kmeans == 4, 0], x[y_kmeans == 4, 1], s=100, c='magenta',
+            label='Cluster 5')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1],
+            s=300, c='yellow', label='Centroids')
+plt.legend()
+plt.title('Cluster of Customers')
+plt.xlabel('Annual Income in k$')
+plt.ylabel('Spending Scores [1-100]')
+plt.show()
